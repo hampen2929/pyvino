a961904545853f516fd3e70310c2acf591eb5fa9
yuya <mochi.no.kimochi.2929@gmail.com>
2019-08-12 22:34:45 +0900

    update: load json

--- vinopy/util/config.py
+++ vinopy/util/config.py
@@ -1,11 +1,38 @@
 
 import configparser
+import json
 
 
-def read_config():
+def load_config():
     config = configparser.ConfigParser()
     config.read('./config.ini', 'UTF-8')
     return config
 
 
-CONFIG = read_config()
+def device_type(device):
+    if device == "CPU":
+        model_fp = 'FP32'
+    else:
+        raise NotImplementedError
+    return model_fp
+
+
+def load_json(file_name):
+    f = open(file_name, 'r')
+    data = json.load(f)
+    f.close()
+    return data
+
+
+def load_task():
+    tasks = load_json('./tasks.json')
+    return tasks
+
+
+CONFIG = load_config()
+DEVICE = CONFIG["MODEL"]["DEVICE"]
+MODEL_DIR = CONFIG["MODEL"]["MODEL_DIR"]
+MODEL_FP = device_type(DEVICE)
+CPU_EXTENSION = CONFIG["MODEL"]["CPU_EXTENSION"]
+
+TASKS = load_task()
