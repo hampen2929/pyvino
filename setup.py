7af7498314eb5ed015887183bdf5faf5005596e4
yuya <mochi.no.kimochi.2929@gmail.com>
2019-08-15 00:38:51 +0900

    add :files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vinopy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='openvino for python',
+    author='hampen',
+    license='Apache',
+)
