d312bba57d61ae5e54b7778ca08dcafa171b7f0a
yuya <mochi.no.kimochi.2929@gmail.com>
2019-12-28 01:48:26 +0900

    udpate: for docker ubuntu 18.04

--- pyvino/model/human_pose_estimation/human_3d_pose_estimator/human_3d_pose_estimator.py
+++ pyvino/model/human_pose_estimation/human_3d_pose_estimator/human_3d_pose_estimator.py
@@ -38,9 +38,9 @@ class Human3DPoseDetector(BaseModel):
     canvas_3d = np.zeros((360, 640, 3), dtype=np.uint8)
 
     plotter = Plotter3d(canvas_3d.shape[:2])
-    canvas_3d_window_name = 'Canvas 3D'
-    cv2.namedWindow(canvas_3d_window_name)
-    cv2.setMouseCallback(canvas_3d_window_name, Plotter3d.mouse_callback)
+    # canvas_3d_window_name = 'Canvas 3D'
+    # cv2.namedWindow(canvas_3d_window_name)
+    # cv2.setMouseCallback(canvas_3d_window_name, Plotter3d.mouse_callback)
     
     def __init__(self):
         self.task = 'estimate_humanpose_3d'
